849c8b0e67aa5755d48f3242d225b796cf1b9ea0
amadorneto <amador_interativa@hotmail.com>
2017-11-21 15:45:53 -0200

    New version of sd-card code

---
+++ sd-card/lib/fatfs/defines.h
@@ -0,0 +1,5 @@
+#define    FATFS_USE_SDIO                0
+#define FATFS_SPI                SPI1
+#define FATFS_SPI_PINSPACK        TM_SPI_PinsPack_1
+#define FATFS_CS_PORT        GPIOB
+#define FATFS_CS_PIN        GPIO_Pin_5
---
+++ sd-card/stmcube/SD_USB_ARM/build/ff.d
@@ -0,0 +1,128 @@
+build/ff.d: Middlewares/Third_Party/FatFs/src/ff.c \
+ Middlewares/Third_Party/FatFs/src/ff.h \
+ Middlewares/Third_Party/FatFs/src/integer.h Inc/ffconf.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ Inc/stm32f4xx_hal_conf.h Inc/main.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ Drivers/CMSIS/Include/core_cm4.h Drivers/CMSIS/Include/core_cmInstr.h \
+ Drivers/CMSIS/Include/cmsis_gcc.h Drivers/CMSIS/Include/core_cmFunc.h \
+ Drivers/CMSIS/Include/core_cmSimd.h \
+ Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sd.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h Inc/bsp_driver_sd.h \
+ Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h \
+ Inc/usbh_conf.h \
+ Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h \
+ Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h \
+ Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h \
+ Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h \
+ Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h \
+ Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Inc/usbh_msc.h \
+ Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Inc/usbh_msc_bot.h \
+ Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Inc/usbh_msc_scsi.h \
+ Middlewares/Third_Party/FatFs/src/diskio.h
+
+Middlewares/Third_Party/FatFs/src/ff.h:
+
+Middlewares/Third_Party/FatFs/src/integer.h:
+
+Inc/ffconf.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+Inc/stm32f4xx_hal_conf.h:
+
+Inc/main.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+Drivers/CMSIS/Include/core_cm4.h:
+
+Drivers/CMSIS/Include/core_cmInstr.h:
+
+Drivers/CMSIS/Include/cmsis_gcc.h:
+
+Drivers/CMSIS/Include/core_cmFunc.h:
+
+Drivers/CMSIS/Include/core_cmSimd.h:
+
+Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sd.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+Inc/bsp_driver_sd.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h:
+
+Inc/usbh_conf.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Inc/usbh_msc.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Inc/usbh_msc_bot.h:
+
+Middlewares/ST/STM32_USB_Host_Library/Class/MSC/Inc/usbh_msc_scsi.h:
+
+Middlewares/Third_Party/FatFs/src/diskio.h:
